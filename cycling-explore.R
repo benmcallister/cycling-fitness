143c73e91aa55c7f74b9a228eb3332334037ccc5
Ben McAllister <7495103+benmcallister@users.noreply.github.com>
2022-03-30 22:30:28 -0400

    Calculated average power per day and graphed it

--- cycling-explore.R
+++ cycling-explore.R
@@ -1,12 +1,62 @@
 library(tidyverse)
 library("trackeR")
-# filepath <- system.file("extdata/tcx/", "2022-03-26_14-00-19", package = "trackeR")
-cycleDF <- readTCX(file = "2022-03-26_14-00-19.tcx", timezone = "GMT")
 
-mean(cycleDF$power)
-str(cycleDF)
+temp <- list.files(pattern = "*.tcx")
 
-p <- ggplot(cycleDF, aes(x=time, y=power)) +
+AllData <-
+  list.files(pattern="*.tcx") %>% 
+  map_df(~readTCX(.))
+
+AllData$date <- as.Date(AllData$time)
+
+# Function to find single-set PRs with time window and exercise as variable
+mean_power <- function(df){
+  df %>% group_by(date) %>% filter(power == mean(power)) %>% 
+    select(date, power) %>% arrange(date)
+}
+
+mean_power(AllData)
+
+# find average ppower for each date
+powerMeans <- AllData %>% group_by(date) %>% summarize(mean(power))
+powerMeans <- rename(powerMeans, meanPower = "mean(power)")
+
+# graph average power
+AvgPowerGraph <- ggplot(powerMeans, aes(x=date, y=meanPower)) +
+  geom_col() + 
+  xlab("")
+AvgPowerGraph
+
+allGraph <- ggplot(AllData, aes(x=time, y=power)) +
   geom_line() + 
   xlab("")
-p
+allGraph
+
+
+
+
+# filepath <- system.file("extdata/tcx/", "2022-03-26_14-00-19", package = "trackeR")
+cycleFeb <- readTCX(file = "2022-02-17_05-54-47.tcx", timezone = "GMT")
+cycleMarch24 <- readTCX(file = "1437503.tcx", timezone = "GMT")
+cycleMarch26 <- readTCX(file = "2022-03-26_14-00-19.tcx", timezone = "GMT")
+
+
+head(cycleMarch24)
+
+mean(cycleMarch$power)
+mean(cycleFeb$power)
+str(cycleMarch)
+
+cycleMarch$powerHR <- cycleMarch$power / cycleMarch$heart_rate
+
+marchGraph <- ggplot(cycleMarch, aes(x=time, y=heart_rate)) +
+  geom_line() + 
+  xlab("")
+marchGraph
+
+cycleFeb$powerHR <- cycleFeb$power / cycleFeb$heart_rate
+
+febGraph <- ggplot(cycleFeb, aes(x=time, y=heart_rate)) +
+  geom_line() + 
+  xlab("")
+febGraph
